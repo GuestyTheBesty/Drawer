23852ac7a2ecebbd08ddf270763719c0250562ad
Guest <guestythebesty841@gmail.com>
2025-05-16 21:14:57 -0400

    erm

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// For the color picker
+const picker = new Picker({
+    parent: document.getElementById('color-picker'),
+    popup: false,
+    color: '#000000', 
+    onDone: function(color) { console.log(color.hex); }
+});
+
 const colorsCircles = document.querySelectorAll('.color-circle');
 
 colorsCircles.forEach(color => {
